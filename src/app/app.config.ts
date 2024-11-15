b579f26bcfc5151f616aa3d5271f0ca1f4f4f5c9
sleepynut <nut.pa@kbtg.tech>
2024-11-15 14:55:41 +0700

    project setup

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,10 +1,31 @@
-import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
+import {
+  ApplicationConfig,
+  isDevMode,
+  provideZoneChangeDetection,
+} from '@angular/core';
 import { provideRouter } from '@angular/router';
 
-import { routes } from './app.routes';
 import { provideClientHydration } from '@angular/platform-browser';
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
+import { provideEffects } from '@ngrx/effects';
+import { provideState, provideStore } from '@ngrx/store';
+import { provideStoreDevtools } from '@ngrx/store-devtools';
+import { routes } from './app.routes';
+import { SearchEffects } from './store/search/search.effects';
+import {
+  searchFeatureKey,
+  reducer as searchReducer,
+} from './store/search/search.reducer';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes), provideClientHydration(), provideAnimationsAsync()]
+  providers: [
+    provideZoneChangeDetection({ eventCoalescing: true }),
+    provideRouter(routes),
+    provideClientHydration(),
+    provideAnimationsAsync(),
+    provideStore(),
+    provideState({ name: searchFeatureKey, reducer: searchReducer }),
+    provideEffects(SearchEffects),
+    provideStoreDevtools({ maxAge: 25, logOnly: !isDevMode() }),
+  ],
 };
