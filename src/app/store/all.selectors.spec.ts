e994ea0db7d800abaeb9b0c1899d044519f8001e
sleepynut <nut.pa@kbtg.tech>
2024-11-15 14:54:41 +0700

    add global state

---
+++ src/app/store/all.selectors.spec.ts
@@ -0,0 +1,7 @@
+
+
+describe('All Selectors', () => {
+  it('should select the feature state', () => {
+    
+  });
+});
---
+++ src/app/store/all.selectors.ts
@@ -0,0 +1,5 @@
+import { SearchTrackResp } from '../models/search-track.model';
+
+export interface AppState {
+  searchTrack: SearchTrackResp;
+}
