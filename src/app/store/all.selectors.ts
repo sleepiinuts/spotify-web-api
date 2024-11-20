e34e39ecffdf0731c5ee82947cc254215fea6753
sleepynut <nut.pa@kbtg.tech>
2024-11-20 10:08:35 +0700

    add selector

--- src/app/store/all.selectors.ts
+++ src/app/store/all.selectors.ts
@@ -1,5 +1,13 @@
+import { createFeatureSelector } from '@ngrx/store';
 import { SearchTrackResp } from '../models/search-track.model';
+import {
+  searchFeatureKey,
+  State as searchState,
+} from './search/search.reducer';
 
 export interface AppState {
   searchTrack: SearchTrackResp;
 }
+
+export const selectSearchTrack =
+  createFeatureSelector<searchState>(searchFeatureKey);
