8c61628d296613c091e9a21fbdc2a56b361d6f35
sleepynut <nut.pa@kbtg.tech>
2024-11-21 16:39:15 +0700

    initial track component

--- src/app/components/track/track.component.ts
+++ src/app/components/track/track.component.ts
@@ -1,4 +1,8 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { Store } from '@ngrx/store';
+import { AppState } from '../../store/all.selectors';
+import { TrackActions } from '../../store/track/track.actions';
 
 @Component({
   selector: 'app-track',
@@ -7,6 +11,16 @@ import { Component } from '@angular/core';
   templateUrl: './track.component.html',
   styleUrl: './track.component.scss',
 })
-export class TrackComponent {
+export class TrackComponent implements OnInit {
   // track = input.required<Track>();
+
+  ngOnInit(): void {
+    this.route.queryParams.subscribe((params) => {
+      let id = params['id'];
+      if (!id) return;
+
+      this.store.dispatch(TrackActions.loadTracks({ id: params['id'] }));
+    });
+  }
+  constructor(private route: ActivatedRoute, private store: Store<AppState>) {}
 }
