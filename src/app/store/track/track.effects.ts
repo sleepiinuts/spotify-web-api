d2bbcefb23ea4d3a0021c214649ba2e683df7a08
sleepynut <nut.pa@kbtg.tech>
2024-11-21 16:40:08 +0700

    create track effect

--- src/app/store/track/track.effects.ts
+++ src/app/store/track/track.effects.ts
@@ -1,11 +1,27 @@
-import { Injectable } from '@angular/core';
-import { Actions, createEffect } from '@ngrx/effects';
-
-
+import { Inject, inject, Injectable } from '@angular/core';
+import { Actions, createEffect, ofType } from '@ngrx/effects';
+import { SpotifyApi } from '@spotify/web-api-ts-sdk';
+import { catchError, from, of, switchMap } from 'rxjs';
+import { SPOTIFY_SDK } from '../../config.injection-token';
+import { TrackActions } from './track.actions';
 
 @Injectable()
 export class TrackEffects {
+  private actions$ = inject(Actions);
 
+  loadTrack$ = createEffect(() =>
+    this.actions$.pipe(
+      ofType(TrackActions.loadTracks),
+      switchMap((props) => {
+        return from(this.sdk.tracks.get(props.id)).pipe(
+          switchMap((track) =>
+            of(TrackActions.loadTracksSuccess({ track: track }))
+          ),
+          catchError((err) => of(TrackActions.loadTracksFailure(err)))
+        );
+      })
+    )
+  );
 
-  constructor(private actions$: Actions) {}
+  constructor(@Inject(SPOTIFY_SDK) private sdk: SpotifyApi) {}
 }
