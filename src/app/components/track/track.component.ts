f3f17c23b0c010ef56ec0be7da26dbcfbe010c38
sleepynut <nut.pa@kbtg.tech>
2024-11-21 19:20:50 +0700

    using destroyRef and fetch by trackId

--- src/app/components/track/track.component.ts
+++ src/app/components/track/track.component.ts
@@ -1,26 +1,38 @@
-import { Component, OnInit } from '@angular/core';
-import { ActivatedRoute } from '@angular/router';
+import { Component, DestroyRef, inject, OnInit } from '@angular/core';
+import { takeUntilDestroyed } from '@angular/core/rxjs-interop';
+import { ActivatedRoute, RouterLink, RouterLinkActive } from '@angular/router';
 import { Store } from '@ngrx/store';
-import { AppState } from '../../store/all.selectors';
+import { Track } from '@spotify/web-api-ts-sdk';
+import { AppState, selectTrack } from '../../store/all.selectors';
 import { TrackActions } from '../../store/track/track.actions';
 
 @Component({
   selector: 'app-track',
   standalone: true,
-  imports: [],
+  imports: [RouterLink, RouterLinkActive],
   templateUrl: './track.component.html',
   styleUrl: './track.component.scss',
 })
 export class TrackComponent implements OnInit {
-  // track = input.required<Track>();
+  track!: Track;
+  readonly destroyRef = inject(DestroyRef);
 
   ngOnInit(): void {
-    this.route.queryParams.subscribe((params) => {
-      let id = params['id'];
-      if (!id) return;
+    // fetch by track id
+    this.route.queryParams
+      .pipe(takeUntilDestroyed(this.destroyRef))
+      .subscribe((params) => {
+        let id = params['id'];
+        if (!id) return;
 
-      this.store.dispatch(TrackActions.loadTracks({ id: params['id'] }));
-    });
+        this.store.dispatch(TrackActions.loadTracks({ id: params['id'] }));
+      });
+
+    // subscribe to track
+    this.store
+      .select(selectTrack)
+      .pipe(takeUntilDestroyed(this.destroyRef))
+      .subscribe((state) => (this.track = state.track));
   }
   constructor(private route: ActivatedRoute, private store: Store<AppState>) {}
 }
