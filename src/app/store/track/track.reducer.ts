43720f3bc9d04d9b81c4bc454e2fb14cc72a5dc9
sleepynut <nut.pa@kbtg.tech>
2024-11-21 16:40:19 +0700

    create track reducer

--- src/app/store/track/track.reducer.ts
+++ src/app/store/track/track.reducer.ts
@@ -1,17 +1,21 @@
 import { createReducer, on } from '@ngrx/store';
+import { Track } from '@spotify/web-api-ts-sdk';
 import { TrackActions } from './track.actions';
 
 export const trackFeatureKey = 'track';
 
 export interface State {
-
+  track: Track;
 }
 
 export const initialState: State = {
-
+  track: <Track>{},
 };
 
 export const reducer = createReducer(
   initialState,
+  on(TrackActions.loadTracksSuccess, (state, props) => ({
+    ...state,
+    track: props.track,
+  }))
 );
-
