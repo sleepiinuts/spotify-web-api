b9462f96dd157a7e5cb464cbc536f2ee16736d19
sleepynut <nut.pa@kbtg.tech>
2024-11-21 20:50:34 +0700

    add loading action

--- src/app/store/track/track.actions.ts
+++ src/app/store/track/track.actions.ts
@@ -1,10 +1,11 @@
-import { createActionGroup, props } from '@ngrx/store';
+import { createActionGroup, emptyProps, props } from '@ngrx/store';
 import { Track } from '@spotify/web-api-ts-sdk';
 
 export const TrackActions = createActionGroup({
   source: 'Track',
   events: {
     'Load Tracks': props<{ id: string }>(),
+    'Load Tracks Loading': emptyProps(),
     'Load Tracks Success': props<{ track: Track }>(),
     'Load Tracks Failure': props<{ error: unknown }>(),
   },
