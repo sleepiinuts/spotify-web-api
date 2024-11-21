494072478ba0962d8ef96144bdc2ee188139a17b
sleepynut <nut.pa@kbtg.tech>
2024-11-21 20:51:32 +0700

    subscribe to isLoading

--- src/app/components/track/track.component.ts
+++ src/app/components/track/track.component.ts
@@ -1,20 +1,26 @@
 import { Component, DestroyRef, inject, OnInit } from '@angular/core';
 import { takeUntilDestroyed } from '@angular/core/rxjs-interop';
+import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
 import { ActivatedRoute, RouterLink, RouterLinkActive } from '@angular/router';
 import { Store } from '@ngrx/store';
 import { Track } from '@spotify/web-api-ts-sdk';
-import { AppState, selectTrack } from '../../store/all.selectors';
+import {
+  AppState,
+  selectTrack,
+  selectTrackLoading,
+} from '../../store/all.selectors';
 import { TrackActions } from '../../store/track/track.actions';
 
 @Component({
   selector: 'app-track',
   standalone: true,
-  imports: [RouterLink, RouterLinkActive],
+  imports: [RouterLink, RouterLinkActive, MatProgressSpinnerModule],
   templateUrl: './track.component.html',
   styleUrl: './track.component.scss',
 })
 export class TrackComponent implements OnInit {
   track!: Track;
+  isLoading = true;
   readonly destroyRef = inject(DestroyRef);
 
   ngOnInit(): void {
@@ -27,6 +33,12 @@ export class TrackComponent implements OnInit {
 
         this.store.dispatch(TrackActions.loadTracks({ id: params['id'] }));
       });
+
+    // TODO: managing loading state
+    this.store
+      .select(selectTrackLoading)
+      .pipe(takeUntilDestroyed(this.destroyRef))
+      .subscribe((isLoading) => (this.isLoading = isLoading));
 
     // subscribe to track
     this.store
