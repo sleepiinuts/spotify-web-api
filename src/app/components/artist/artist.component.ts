57766f4455d6e54370c6cddc5a9611993d0624a0
sleepynut <nut.pa@kbtg.tech>
2024-11-22 23:18:51 +0700

    initial artist component

--- src/app/components/artist/artist.component.ts
+++ src/app/components/artist/artist.component.ts
@@ -1,12 +1,19 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Store } from '@ngrx/store';
+import { AppState } from '../../store/all.selectors';
+import { ArtistActions } from '../../store/artist/artist.actions';
 
 @Component({
   selector: 'app-artist',
   standalone: true,
   imports: [],
   templateUrl: './artist.component.html',
-  styleUrl: './artist.component.scss'
+  styleUrl: './artist.component.scss',
 })
-export class ArtistComponent {
+export class ArtistComponent implements OnInit {
+  constructor(private store: Store<AppState>) {}
 
+  ngOnInit(): void {
+    this.store.dispatch(ArtistActions.load());
+  }
 }
