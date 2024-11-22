d925815af8cefa38cc9bae1b36033c3680bde69d
sleepynut <nut.pa@kbtg.tech>
2024-11-22 23:18:33 +0700

    add artist selectors

--- src/app/store/all.selectors.ts
+++ src/app/store/all.selectors.ts
@@ -2,6 +2,10 @@ import { createFeatureSelector, createSelector } from '@ngrx/store';
 import { Track } from '@spotify/web-api-ts-sdk';
 import { SearchTrackResp } from '../models/search-track.model';
 import {
+  artistFeatureKey,
+  State as artistState,
+} from './artist/artist.reducer';
+import {
   searchFeatureKey,
   State as searchState,
 } from './search/search.reducer';
@@ -10,6 +14,7 @@ import { trackFeatureKey, State as trackState } from './track/track.reducer';
 export interface AppState {
   searchTrack: SearchTrackResp;
   track: Track;
+  artist: artistState;
 }
 
 export const selectSearchTrack =
@@ -23,4 +28,11 @@ export const selectTrack = createFeatureSelector<trackState>(trackFeatureKey);
 export const selectTrackLoading = createSelector(
   selectTrack,
   (state) => state.isloading
+);
+
+export const selectArtist =
+  createFeatureSelector<artistState>(artistFeatureKey);
+export const selectArtistIds = createSelector(
+  selectArtist,
+  (state) => state.ids
 );
