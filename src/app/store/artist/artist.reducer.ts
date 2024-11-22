9b8f83964122738c9f4420fbbf39fa10a38f24c8
sleepynut <nut.pa@kbtg.tech>
2024-11-22 23:20:30 +0700

    initial artist reducer

---
+++ src/app/store/artist/artist.reducer.ts
@@ -0,0 +1,33 @@
+import { createReducer, on } from '@ngrx/store';
+import { Artist } from '@spotify/web-api-ts-sdk';
+import { ArtistActions } from './artist.actions';
+
+export const artistFeatureKey = 'artist';
+
+export interface State {
+  ids: string[];
+  artist: Artist[];
+  isLoading: boolean;
+}
+
+export const initialState: State = {
+  ids: [],
+  artist: [],
+  isLoading: false,
+};
+
+export const reducer = createReducer(
+  initialState,
+  on(ArtistActions.ids, (state, props) => ({
+    ...state,
+    ids: props.ids,
+  })),
+  on(ArtistActions.loading, (state) => ({
+    ...state,
+    isLoading: true,
+  })),
+  on(ArtistActions.loadSuccess, (state, props) => ({
+    ...state,
+    artist: props.artists,
+  }))
+);
