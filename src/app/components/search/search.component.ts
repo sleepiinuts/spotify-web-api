cb10f937598c5d4e0c03befbb8e88605549e9906
sleepynut <nut.pa@kbtg.tech>
2024-11-22 14:32:30 +0700

    concat artists' ids

--- src/app/components/search/search.component.ts
+++ src/app/components/search/search.component.ts
@@ -22,7 +22,7 @@ import { MatIconModule } from '@angular/material/icon';
 import { MatInputModule } from '@angular/material/input';
 import { RouterLink, RouterLinkActive } from '@angular/router';
 import { Store } from '@ngrx/store';
-import { Track } from '@spotify/web-api-ts-sdk';
+import { SimplifiedArtist, Track } from '@spotify/web-api-ts-sdk';
 import { WINDOW } from '../../config.injection-token';
 import { ListArtistsPipe } from '../../pipe/list-artists.pipe';
 import { AppState, selectSearchTrack } from '../../store/all.selectors';
@@ -64,6 +64,10 @@ export class SearchComponent implements OnInit {
     this.store.dispatch(
       SearchActions.searchsTrack({ q: this.searchForm.value.txtInput! })
     );
+  }
+
+  extractId(artists: SimplifiedArtist[]): string {
+    return artists.map((a) => a.id).join(',');
   }
 
   ngOnInit(): void {
