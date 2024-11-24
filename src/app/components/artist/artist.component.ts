8eba32cfc997a34771c20fa46b8cda3abc726022
sleepynut <nut.pa@kbtg.tech>
2024-11-24 19:28:50 +0700

    initial artist control

--- src/app/components/artist/artist.component.ts
+++ src/app/components/artist/artist.component.ts
@@ -1,19 +1,45 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, DestroyRef, OnInit } from '@angular/core';
+import { takeUntilDestroyed } from '@angular/core/rxjs-interop';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
 import { Store } from '@ngrx/store';
-import { AppState } from '../../store/all.selectors';
+import { Artist } from '@spotify/web-api-ts-sdk';
+import { AppState, selectArtistInfo } from '../../store/all.selectors';
 import { ArtistActions } from '../../store/artist/artist.actions';
 
 @Component({
   selector: 'app-artist',
   standalone: true,
-  imports: [],
+  imports: [MatButtonModule, MatIconModule],
   templateUrl: './artist.component.html',
   styleUrl: './artist.component.scss',
 })
 export class ArtistComponent implements OnInit {
-  constructor(private store: Store<AppState>) {}
+  public idx = 0;
+  public artists!: Artist[];
+
+  prev() {
+    if (this.idx > 0) {
+      this.idx -= 1;
+    }
+
+    console.log(`pos: ${this.idx}, artist.length: ${this.artists.length}`);
+  }
+
+  next() {
+    if (this.idx < this.artists.length - 1) {
+      this.idx += 1;
+    }
+    console.log(`pos: ${this.idx}, artist.length: ${this.artists.length}`);
+  }
 
   ngOnInit(): void {
     this.store.dispatch(ArtistActions.load());
+
+    this.store
+      .select(selectArtistInfo)
+      .pipe(takeUntilDestroyed(this.destroyRef))
+      .subscribe((artists) => (this.artists = artists));
   }
+  constructor(private store: Store<AppState>, private destroyRef: DestroyRef) {}
 }
