559a3795353434dc15c04ead07a3334b7e0f4bb0
sleepynut <nut.pa@kbtg.tech>
2024-12-03 16:03:28 +0700

    1st draft of placeholder component

--- src/app/components/shared/placeholder/placeholder.component.ts
+++ src/app/components/shared/placeholder/placeholder.component.ts
@@ -1,9 +1,14 @@
-import { CommonModule } from '@angular/common';
+import { CommonModule, DOCUMENT } from '@angular/common';
 import {
   ChangeDetectionStrategy,
+  ChangeDetectorRef,
   Component,
+  Inject,
   Input,
+  OnChanges,
   OnInit,
+  SimpleChanges,
+  ViewContainerRef,
 } from '@angular/core';
 import { SkeletonPlaceholderConfig } from './placeholder.config.type';
 
@@ -15,13 +20,19 @@ import { SkeletonPlaceholderConfig } from './placeholder.config.type';
   styleUrl: './placeholder.component.scss',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class PlaceholderComponent implements OnInit {
+export class PlaceholderComponent implements OnInit, OnChanges {
   @Input() animation: SkeletonPlaceholderConfig['animation'];
   @Input() count: SkeletonPlaceholderConfig['count'];
   @Input() theme: SkeletonPlaceholderConfig['theme'];
   @Input() loadingText: SkeletonPlaceholderConfig['loadingText'];
-  items!: any[];
-  constructor() {
+  @Input() completeIndexes: number[];
+  @Input() id: string;
+  loadings!: boolean[];
+  constructor(
+    private cd: ChangeDetectorRef,
+    @Inject(DOCUMENT) document: Document,
+    private vcr: ViewContainerRef,
+  ) {
     const {
       animation = 'false',
       count = 1,
@@ -33,12 +44,52 @@ export class PlaceholderComponent implements OnInit {
     this.count = count;
     this.theme = theme;
     this.loadingText = loadingText;
+
+    this.completeIndexes = [];
+    this.id = '';
+
+    // console.log(`constructor: ${this.loadings}`);
+  }
+
+  toggleIsLoading(idx: number) {
+    setTimeout(() => {
+      this.loadings[idx] = false;
+      document.getElementById(`${idx}`)?.classList.add('show');
+      this.cd.detectChanges();
+    }, 3000);
+
+    // setTimeout(() => {
+    //   this.completeIndexes = [...this.completeIndexes, idx];
+    //   console.log(`isLoading: ${this.loadings}`);
+    //   this.cd.detectChanges();
+    // }, 3000);
+  }
+
+  getId(idx: number): string {
+    return `${this.id}-${idx}`;
   }
 
   ngOnInit(): void {
     // initialize value received through @input
-    this.items = new Array(this.count);
+    this.loadings = Array.from({ length: this.count }, () => true);
+    // console.log(`onInit: ${this.loadings}`);
+  }
 
-    // console.log(`animation: ${this.animation}`);
+  ngOnChanges(changes: SimpleChanges): void {
+    // console.log(`onChange: ${this.loadings}`);
+
+    if (
+      changes['completeIndexes']?.previousValue !==
+      changes['completeIndexes']?.currentValue
+    ) {
+      this.completeIndexes.forEach((idx) => {
+        console.log(`toggle placeholder id: ${idx}`);
+        document.getElementById(`${this.id}-${idx}`)?.classList.add('hide');
+        // this.el.nativeElement.querySelector(`${idx}`).classList.add('hide');
+      });
+
+      document.getElementById(`skeleton-${this.id}`)?.classList.add('hide');
+      this.vcr.clear(); //how to remove self from dom ??
+    }
   }
 }
